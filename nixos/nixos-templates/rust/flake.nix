952b6baf0d4152fdf15baa78a9263ddbce900ce9
greed <local>
2025-11-28 16:22:58 +0530

    feat: simplified rust flake template

--- nixos/nixos-templates/rust/flake.nix
+++ nixos/nixos-templates/rust/flake.nix
@@ -3,12 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
     naersk = {
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -21,61 +19,45 @@
     naersk,
     rust-overlay,
   }: let
-    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-
-    # Generate pkgs for each system
-    pkgsFor = system:
-      import nixpkgs {
-        inherit system;
-        overlays = [(import rust-overlay)];
-      };
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [(import rust-overlay)];
+    };
+    naerskLib = pkgs.callPackage naersk {};
+    # rustToolchain = pkgs.rust-bin.stable.latest.default;
+    rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
   in {
-    packages = forAllSystems (system: let
-      pkgs = pkgsFor system;
-      naerskLib = pkgs.callPackage naersk {};
-
-      # rustToolchain = pkgs.rust-bin.stable.latest.default;
-      rustToolchain =
-        pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-    in {
-      default =
-        (naerskLib.override {
-          cargo = rustToolchain;
-          rustc = rustToolchain;
-        }).buildPackage {
-          src = ./.;
-          nativeBuildInputs = [pkgs.pkg-config];
-          buildInputs = [
-            pkgs.glib
-            pkgs.openssl
-            # pkgs.sqlite
-          ];
-        };
-    });
-
-    devShells = forAllSystems (system: let
-      pkgs = pkgsFor system;
-      rustToolchain = pkgs.rust-bin.nightly.latest.default;
-    in {
-      default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          rustToolchain
-          rust-analyzer
-          cargo-nextest
-          cargo-audit
-          bacon
-          cargo-udeps
-
-          pkg-config
-          glib
-          openssl
+    packages.${system}.default =
+      (naerskLib.override {
+        cargo = rustToolchain;
+        rustc = rustToolchain;
+      }).buildPackage {
+        src = ./.;
+        nativeBuildInputs = [pkgs.pkg-config];
+        buildInputs = [
+          pkgs.glib
+          pkgs.openssl
+          # pkgs.sqlite
         ];
-
-        # Optional: set environment variables
-        # Uncomment if you are having issues with go to definitions
-        # RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
       };
-    });
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        (rust-bin.nightly.latest.default)
+        bacon
+        cargo-nextest
+        cargo-audit
+        cargo-udeps
+        glib
+        openssl
+        pkg-config
+        rust-analyzer
+      ];
+
+      # Optional: set environment variables
+      # Uncomment if you are having issues with go to definitions
+      # RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+    };
   };
 }
